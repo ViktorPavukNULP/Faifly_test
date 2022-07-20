2e180accc3c98078ba77af067264de0250f55d41
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-07-20 15:25:37 +0300

    database, requirements

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'faifly',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
